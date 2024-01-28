4a958efa3f463849db5787d1978dec401dc2bd4e
hamid <h.rezaei@asax.ir>
2024-01-28 22:00:06 +0330

    fix: handler bug

--- internal/infra/http/handler/handler.go
+++ internal/infra/http/handler/handler.go
@@ -3,17 +3,17 @@ package handler
 import "github.com/Hamid-Rezaei/goMessenger/internal/infra/repository"
 
 type Handler struct {
-	userRepo repository.UserRepo,
-	contactRepo repository.ContactRepo,
-	chatRepo repository.ChatRepo,
+	userRepo    repository.UserRepo
+	contactRepo repository.ContactRepo
+	chatRepo    repository.ChatRepo
 	messageRepo repository.MessageRepo
 }
 
 func NewHandler(ur repository.UserRepo, cr repository.ContactRepo, chr repository.ChatRepo, mr repository.MessageRepo) *Handler {
 	return &Handler{
-		userRepo: ur,
+		userRepo:    ur,
 		contactRepo: cr,
-		chatRepo: chr,
-		messageRepo: mr
+		chatRepo:    chr,
+		messageRepo: mr,
 	}
 }
